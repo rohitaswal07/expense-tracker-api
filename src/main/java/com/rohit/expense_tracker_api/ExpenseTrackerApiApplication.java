b2249a09bb59cb19556d942bded08169938bcb3d
Rohit <rohitaswal2407@gmail.com>
2026-08-01 23:25:21 +0530

    Add Swagger API documentation

--- src/main/java/com/rohit/expense_tracker_api/ExpenseTrackerApiApplication.java
+++ src/main/java/com/rohit/expense_tracker_api/ExpenseTrackerApiApplication.java
@@ -1,9 +1,23 @@
 package com.rohit.expense_tracker_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+			title = "Expense Tracker OPEN API",
+			version = "1.0.0",
+			description = "Expense Tracker OPEN API documentation"
+		),
+		servers = @Server(
+			url = "http://localhost:8080",
+				description = "Expense Tracker OPEN API url"
+		)
+)
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rohit/expense_tracker_api/controller/ExpenseController.java
+++ src/main/java/com/rohit/expense_tracker_api/controller/ExpenseController.java
@@ -2,6 +2,8 @@ package com.rohit.expense_tracker_api.controller;
 
 import com.rohit.expense_tracker_api.model.Expense;
 import com.rohit.expense_tracker_api.service.ExpenseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/expenses")
+
+@Tag(name = "ExpenseController", description = "Manage Expenses")
 public class ExpenseController {
 
 private final ExpenseService expenseService;
@@ -21,12 +25,15 @@ private final ExpenseService expenseService;
         this.expenseService = expenseService;
     }
 
+
+    @Operation(summary = "Add an Expense")
     @PostMapping
     public ResponseEntity<Expense> addAnExpense(@Valid @RequestBody Expense expense){
      Expense savedExpense= this.expenseService.addExpense(expense);
      return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
 
+    @Operation(summary = "View Expenses")
     @GetMapping
     public List<Expense> viewAllExpenses(@RequestParam(required = false) String category){
         if (category!=null){
@@ -36,16 +43,20 @@ private final ExpenseService expenseService;
     }
 
 
+    @Operation(summary = "Calculate total expenses")
     @GetMapping(value = "/total")
     public BigDecimal calculateTotalExpenses(){
         return this.expenseService.calculateTotal();
     }
 
+
+    @Operation(summary = "Calculate total expenses by category")
     @GetMapping(value = "/total/{category}")
     public BigDecimal calculateTotalExpensesByCategory(@PathVariable String category){
         return this.expenseService.calculateCategoryTotal(category);
     }
 
+    @Operation(summary = "Delete an expense")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<String> deleteAnExpense(@PathVariable Long id){
         this.expenseService.deleteExpense(id);
